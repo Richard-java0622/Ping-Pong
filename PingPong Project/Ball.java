dae1ec7ac570257b67a4ac05675d22aa313507a0
Richard-java0622 <r1ch6rd.o4t3ga@gmail.com>
2024-11-11 11:13:36 -0600

    Add files via upload

---
+++ PingPong Project/Ball.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle{
+
+    Random random;
+    int xVelocity;
+    int yVelocity;
+    //how fast it is going to move
+    
+    Ball() {
+
+    }
+
+    public void setXDirection(int randomXDirection) {
+
+    }
+    public void setYDirection(int randomYDirection) {
+
+    }
+    public void move() {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    // will move the ball in a random x and y position
+    
+}
---
+++ PingPong Project/PongGame.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+		GameFrame frame = new GameFrame();
+    }
+}
+
+// This class should be done, but it MIGHT be modify later in the video, idk tho :O
