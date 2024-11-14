01150f2f004171b78a1c2d9c1b0724101d353e05
LisanTemp <lisant2005@gmail.com>
2024-11-13 23:05:24 -0600

    Update Ball.java

--- PingPong Project/Ball.java
+++ PingPong Project/Ball.java
@@ -8,6 +8,7 @@ public class Ball extends Rectangle{
     Random random;
     int xVelocity;
     int yVelocity;
+    int initalSpeed = 3;
     //how fast it is going to move
     
     Ball(int x, int y, int width, int height ) {
@@ -16,12 +17,12 @@ public class Ball extends Rectangle{
         int randomXDirection = random.nextInt(2);
         if(randomXDirection == 0)
             randomXDirection--;
-        setXDirection(randomXDirection);
+        setXDirection(randomXDirection * initialSpeed);
 
          int randomYDirection = random.nextInt(2);
         if(randomYDirection == 0)
             randomYDirection--;
-        setYDirection(randomYDirection);
+        setYDirection(randomYDirection * initialSpeed);
 
     }
 
